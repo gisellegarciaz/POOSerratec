0d35d22dd9d776df5b6bc4a6ac07b47292b651cd
Giselle Garcia <gisellepagarcia@gmail.com>
2025-09-08 21:35:02 -0300

    Commit Aula03_2

---
+++ Aula03_Exercicios/src/Aula03_Exercicios_Switch_TimesFutebol.java
@@ -0,0 +1,23 @@
+/* 1) Criar uma variável time.
+ * Agregar times de dois estados em um switch */
+
+public class Aula03_Exercicios_Switch_TimesFutebol {
+
+	public static void main(String[] args) {
+		String time = "";
+		String estado = "RJ";
+		
+		switch (estado) {
+		case "RJ": 
+			time = "Flamengo, Vasco, Botafogo, Fluminense";
+			break;
+		case "SP":
+			time = "Corintias, Plameiras, Santos, São Paulo";
+			break;
+		default: 
+			System.out.println("Estado inválido.");
+		}
+		
+		System.out.println("Os times do estado " + estado + " são: " + time);
+	}
+}
