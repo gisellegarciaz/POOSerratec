b174d93c8a0f937698aae1f89f320051ad747e50
Giselle Garcia <gisellepagarcia@gmail.com>
2025-09-19 12:43:42 -0300

    Commits Aula03 Ex6

---
+++ Aula03_Exerc6_GiselleG/src/AntecessorSucessor.java
@@ -0,0 +1,24 @@
+//Aluna: Giselle P A Garcia
+
+import java.util.Scanner;
+
+public class AntecessorSucessor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Digite um número inteiro:");
+        int numero = scanner.nextInt();
+
+        
+        int antecessor = numero - 1;
+        int sucessor = numero + 1;
+
+    
+        System.out.println("O antecessor de " + numero + " é: " + antecessor);
+        System.out.println("O sucessor de " + numero + " é: " + sucessor);
+
+        scanner.close();
+    }
+}
+
