8a50c83729bbee037a19414575906ca106733d48
Giselle Garcia <gisellepagarcia@gmail.com>
2025-09-22 12:26:13 -0300

    Commit Aula02 - 2

---
+++ Aula02_Exerc2_GiselleG/src/MainTabuada.java
@@ -0,0 +1,17 @@
+
+public class MainTabuada {
+
+	    public static void main(String[] args) {
+	        int numero = 2;
+	        int multiplicador = 1;
+
+	        System.out.println("Tabela de Multiplicação para o número " + numero + ": ");
+	        while (multiplicador <= 10) {
+	            int resultado = numero * multiplicador;
+	            System.out.println(numero + " x " + multiplicador + " = " + resultado);
+	            multiplicador++;
+	        }
+	    }
+	}
+	
+
